fd1b9a1c9ab7d2c0480e3eda124d5dfd9f4959d0
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-04-22 10:28:48 +0000

    [finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 end

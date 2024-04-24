7d01c57a73be97b3e34dd15483168b795087e780
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-04-24 21:12:22 +0000

    [finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -17,7 +17,13 @@ class PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
   end
-  
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end
+
   private
 
   def post_image_params

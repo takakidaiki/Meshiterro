b85d405060616e04752a292eb13869f6a6cc29e0
daikitakaki <162309554+takakidaiki@users.noreply.github.com>
2024-05-22 09:19:48 +0000

    [finish]5-3

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardsController < ApplicationController
+  layout 'admin' # ここを追加
+  before_action :authenticate_admin!
   def index
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,17 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    root to: "homes#top"
+    get 'homes/about' => 'homes#about', as: 'about'
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorite, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
-  get 'homes/about' => 'homes#about', as: 'about'
 end
